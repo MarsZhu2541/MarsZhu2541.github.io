fe347856075cbda57920f54bf276304c7f30c36b
MarsZhu2541 <74281485+MarsZhu2541@users.noreply.github.com>
2022-11-11 16:35:23 +0800

    Update main.js

--- static/js/main.js
+++ static/js/main.js
@@ -57,11 +57,28 @@ createApp({
             return hexMap
         
         },
+        async getEquipMap() {
+            rawData= {}
+            equipMap = {}
+            await axios.get('https://game.gtimg.cn/images/lol/act/img/tft/js/equip.js')
+                .then(res => {
+                    rawData = res.data.data
+                })
+                .catch(err => {
+                    console.log('错误' + err)
+                })
+            for(i in rawData){
+               equipMap[rawData[i].equipId] =rawData[i]
+            }
+            return equipMap
+        
+        },
         async addCustomData(chessList) {
             let lineupList = this.lineupList
             let chessMap = {};
             let hexMap = await this.getHexMap()
-
+            let equipMap = await this.getEquipMap()
+            // console.log(equipMap)
             for (i in chessList) {
                 chess = chessList[i]
                 chessMap[chess['chessId']] = chess
@@ -70,8 +87,21 @@ createApp({
             for (i in lineupList) {
                 lineup = lineupList[i]
                 //enhance lineup info
+
+                orderStr=lineup.equipment_order
+                orderArr = lineup.equipment_order.split(',')
+                while(orderStr[orderStr.length-1]==','){
+                    orderStr = orderStr.substr(0,orderStr.length-1)
+                    orderArr.pop()
+                }
+                for(h in orderArr){
+                    orderStr = orderStr.replace(orderArr[h],equipMap[parseInt(orderArr[h])].name)   
+                }
+                orderStr = orderStr.replaceAll(',','>')
+
                 lineup.info= "D牌节奏： "+lineup.d_time+"\n"
                 +"早期玩法： "+lineup.early_info+"\n"
+                +"抢装顺序： "+orderStr+"\n\n"
                 +"克制阵容： "+lineup.enemy_info+"\n\n"
                 +"装备推荐： "+lineup.equipment_info+"\n\n"
                 +"海克斯推荐： "+lineup.hex_info+"\n"
@@ -90,7 +120,6 @@ createApp({
                         hero_id = parseInt(position['hero_id'])
                     }
                     hero = chessMap[hero_id]
-                    console.log(hero)
                     if (!(hero == undefined)) {
 
                         position['price'] = hero.price
@@ -103,6 +132,16 @@ createApp({
                         position['is_3_star'] = level_3_heros.includes(hero_id)
                         position['hero_image'] = "https://game.gtimg.cn/images/lol/act/img/tft/champions/" + hero.name
                         position['price'] = hero.price
+                        position['chessPriceClass'] = "component-champion cost"+hero.price +" champion-main"
+                        position['equip_image_list']=[]
+                        position.equipment_id = position.equipment_id.split(',')
+                        for(k in position.equipment_id){
+                            equipID = position.equipment_id[k] 
+                            if(!(equipMap[equipID]==undefined)){
+                                position['equip_image_list'].push(equipMap[equipID].imagePath)  
+                            }
+                            
+                        }
                     }
                 }
                 //enhance hex data
