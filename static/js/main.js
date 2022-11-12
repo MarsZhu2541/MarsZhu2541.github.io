faef0d06e5a9aa0b900be774d66557d11d96702a
MarsZhu2541 <74281485+MarsZhu2541@users.noreply.github.com>
2022-11-12 17:38:16 +0800

    Update main.js

--- static/js/main.js
+++ static/js/main.js
@@ -3,7 +3,7 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-            cacheTime: 3600000,
+            cacheTime: 0,
             lineupList: [],
         }
     },
@@ -12,7 +12,7 @@ createApp({
             console.log("no cache")
             this.getLineupList()
         } else {
-            console.log("use cache")    
+            console.log("use cache")
             this.lineupList = JSON.parse(localStorage.getItem("lineuplist"))
         }
     },
@@ -22,7 +22,6 @@ createApp({
 
             axios.get('https://game.gtimg.cn/images/lol/act/tftzlkauto/json/totalLineupJson/lineup_total.json?v=2779984')
                 .then(res => {
-                    // console.log(res.data)
                     that.handleData(res.data)
                 })
                 .catch(err => {
@@ -34,10 +33,14 @@ createApp({
             let that = this
             let lineupList = this.lineupList
             let lineup_list = data["lineup_list"]
-            details = []
+            lineup_list.sort(this.compareByQuality)
+            lineup_list.sort(this.compareBySortID)
             for (i in lineup_list) {
                 lineup = lineup_list[i]
-                lineupList.push(JSON.parse(lineup['detail'].replace(/\n/g, "\\n").replace(/\r/g, "\\r")))
+                lineUpDetail = JSON.parse(lineup['detail'].replace(/\n/g, "\\n").replace(/\r/g, "\\r"))
+                lineUpDetail.quality =  lineup.quality
+                lineupList.push(lineUpDetail)
+                // console.log(lineUpDetail.line_name+" "+lineup.channel+" "+lineup.quality+" "+lineup.sortID)
             }
 
             axios.get('https://game.gtimg.cn/images/lol/act/img/tft/js/chess.js')
@@ -106,7 +109,8 @@ createApp({
                 }
                 orderStr = orderStr.replaceAll(',', '>')
 
-                lineup.info = "D牌节奏： " + lineup.d_time + "\n"
+                lineup.info = "阵容强度： " + lineup.quality + "\n"
+                    + "D牌节奏： " + lineup.d_time + "\n"
                     + "早期玩法： " + lineup.early_info + "\n"
                     + "抢装顺序： " + orderStr + "\n\n"
                     + "克制阵容： " + lineup.enemy_info + "\n\n"
@@ -162,12 +166,39 @@ createApp({
 
                 }
             }
-            localStorage.setItem("timeLoadLineuplist", new Date().valueOf().toString())
-            localStorage.setItem("lineuplist", JSON.stringify(this.lineupList) )
             console.log(lineupList)
+            localStorage.setItem("timeLoadLineuplist", new Date().valueOf().toString())
+            localStorage.setItem("lineuplist", JSON.stringify(this.lineupList))
         },
         showInfo(info) {
             alert(info)
+        },
+        compareByQuality(a, b) {
+            if(a.quality==""||a.quality=="0"){
+                a.quality = "C"
+            }
+            if(b.quality==""||b.quality=="0"){
+                b.quality = "C"
+            }
+            qualityMap= {"S":0,"A":1,"B":2,"C":3,"":100}
+            a_tag = qualityMap[a.quality]
+            b_tag = qualityMap[b.quality]
+            if (a_tag < b_tag) {
+                return -1;
+            } else if (a_tag > b_tag) {
+                return 1;
+            }
+            return 0;
+        },
+        compareBySortID(a, b) {
+            aID =parseInt(a.sortID)
+            bID =parseInt(b.sortID)
+            if (aID<bID) {
+                return 1;
+            } else if (aID>bID) {
+                return -1;
+            }
+            return 0;
         }
     },
 }).mount('#app')
